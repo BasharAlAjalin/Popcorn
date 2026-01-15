d21200c48508f46a18d7805061d77879a74b134d
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-01-15 06:47:12 +0200

    Fixing the prop drilling by component composition

--- src/App.jsx
+++ src/App.jsx
@@ -51,20 +51,29 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <>
-      <Navbar />
-      <Main />
+      <Navbar>
+        <Search />
+        <NumResults movies={movies} />{" "}
+      </Navbar>
+      <Main>
+        <ListBox>
+          <ListOfMovies movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function Navbar() {
+function Navbar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults />
+      {children}
     </nav>
   );
 }
@@ -90,23 +99,18 @@ function Search() {
     />
   );
 }
-function NumResults() {
+function NumResults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
-function Main() {
-  return (
-    <main className="main">
-      <ListBox />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox() {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -116,14 +120,12 @@ function ListBox() {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <ListOfMovies />}
+      {isOpen1 && children}
     </div>
   );
 }
 
-function ListOfMovies() {
-  const [movies, setMovies] = useState(tempMovieData);
-
+function ListOfMovies({ movies }) {
   return (
     <ul className="list">
       {movies?.map((movie) => (
