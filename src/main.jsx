e8525486426757470cf1c7eebfc1e085e5b8549c
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-01-18 12:27:36 +0200

    Buildin useEffect hock for movies

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
+import "./index.css";
+import App from "./App.jsx";
 import StarsRating from "./StarsRating";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
+    <App />
     <StarsRating maxRating={5} />
-  </StrictMode>
+  </StrictMode>,
 );
